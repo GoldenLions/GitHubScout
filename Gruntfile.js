a6b98c06858082857388b04d9051e2645e8fe542
wykhuh <wykhuh@users.noreply.github.com>
2014-08-28 22:56:24 -0700

    (feat) Start Gruntfile; awaiting file directory path information.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,134 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+
+      dist: {
+        src: [''],
+        dest: ''
+      }
+    },
+
+    uglify: {
+      my_target: {
+        files: {}
+      }
+    },
+
+    cssmin: {
+      options: {
+        keepSpecialComments: 0
+      },
+      dist: {
+        files: {
+          '':''
+        }
+      }
+    },
+
+    jshint: {
+      all: [''],
+      options: {
+        force: 'true',
+        jshintrc: '.jshintrc',
+        ignores: ['']
+      }
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['']
+      }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'server.js'
+      }
+    },
+
+    shell: {
+      prodServer: {
+        command: 'git push azure master',
+        options: {
+          stdout: true,
+          stderr: true,
+          failOnError: true
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: [''],
+        tasks: ['concat', 'uglify']
+      },
+      css: {
+        files: '',
+        tasks: ['cssmin']
+      }
+    }
+
   });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-nodemon');
+    
+
+  ////////////////////////////////////////////////////
+  // Main grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('server-dev', function (target) {
+    // Running nodejs in a different process and displaying output on the main console
+    var nodemon = grunt.util.spawn({
+         cmd: 'grunt',
+         grunt: true,
+         args: 'nodemon'
+    });
+    nodemon.stdout.pipe(process.stdout);
+    nodemon.stderr.pipe(process.stderr);
+
+    grunt.task.run([ 'watch' ]);
+  });
+
+
+  grunt.registerTask('test', [
+    'jshint',
+    'mochaTest'
+  ]);
+
+  grunt.registerTask('build', [
+    'concat',
+    'uglify',
+    'cssmin'
+  ]);
+
+  grunt.registerTask('upload', function(n) {
+    if(grunt.option('prod')){
+      grunt.task.run(['shell:prodServer']);
+    } else {
+      grunt.task.run(['server-dev']);
+    }
+  });
+
+  grunt.registerTask('deploy', [
+    'test',
+    'build',
+    'upload'
+  ]);
+
 };
