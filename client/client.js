43957c1bd70a67f4f761b78dbcfbaadfbf2e64be
walkerkellywalker@gmail.com <walkerkellywalker@gmail.com>
2014-08-29 16:01:13 -0700

    Added angular config and renamed some files

--- client/client.js
+++ client/client.js
@@ -1,3 +1,24 @@
 var githubScout = angular.module('githubScout', [
 	'ui-router'
 ])
+
+.config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: 'client/home/home.html',
+			controller: 'HomeController'
+		})
+		.state('user', {
+			url: '/user',
+			templateUrl: 'client/user/user.html',
+			controller: 'UserController'
+		})
+		.state('language', {
+			url: '/language',
+			templateUrl: 'client/language/language.html',
+			controller: 'LanguageController'
+		});
+
+	$urlRouterProvider.otherwise('/');
+})
