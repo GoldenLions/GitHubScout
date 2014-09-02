e3b73b0a7799d5497d7b153a6c497d5f26460654
Chun Hao Wang <chunhao137@gmail.com>
2014-09-02 16:13:37 -0700

    Fixed axes on user graph

--- client/user/user.js
+++ client/user/user.js
@@ -8,17 +8,17 @@ angular.module('githubscout.user', ['ui.router'])
 .directive('linearChart', function($window){
    return{
       restrict:'EA',
-      template:"<svg width='850' height='200'></svg>",
+      template:"<svg width='1200' height='400'></svg>",
        link: function(scope, elem, attrs){
-           var salesDataToPlot=scope.userdata.data;
+           var dataPlot=scope.userdata.data;
 
-          console.log("this is the scope",salesDataToPlot)
+          console.log("this is the scope",dataPlot)
            var padding = 20;
            var pathClass="path";
            var xScale, yScale, xAxisGen, yAxisGen
 
            var d3 = $window.d3;
-           console.log("window",$window)
+           //console.log("window",$window)
 
            var rawSvg=elem.find('svg');
            var svg = d3.select(rawSvg[0]);
@@ -29,39 +29,30 @@ angular.module('githubscout.user', ['ui.router'])
            function setChartParameters(){
 
                xScale = d3.scale.linear()
-                   .domain([0, salesDataToPlot.length-1])
+                   .domain([0, dataPlot.length-1])
                    .range([padding + 5, rawSvg.attr("width") - padding]);
 
                yScale = d3.scale.linear()
-                   .domain([0, d3.max(salesDataToPlot, function (d) {
+                   .domain([0, d3.max(dataPlot, function (d) {
 
-                       return d.count;
+                       return (d.count*2.4);
                    })])
                    .range([rawSvg.attr("height") - padding, 0]);
 
                xAxisGen = d3.svg.axis()
                    .scale(xScale)
-                   .tickFormat(function(d,i) { return salesDataToPlot[d].date})
+                   .tickFormat(function(d,i) { return dataPlot[d].date})
                    .orient("bottom")
-                   .ticks(salesDataToPlot.length - 1);
+                   .ticks(dataPlot.length - 1);
 
-                   console.log("xscale",xAxisGen)
+                   //console.log("xscale",xAxisGen)
 
 
                yAxisGen = d3.svg.axis()
                    .scale(yScale)
                    .orient("left")
-                   .ticks(4)
-            
-
-               // lineFun = d3.svg.line()
-               //     .x(function (d) {
-               //         return xScale(d.hour);
-               //     })
-               //     .y(function (d) {
-               //         return yScale(d.sales);
-               //     })
-               //     .interpolate("basis");
+                   .ticks(5)
+          
            }
          
          function drawLineChart() {
@@ -71,24 +62,31 @@ angular.module('githubscout.user', ['ui.router'])
 
                svg.append("svg:g")
                    .attr("class", "x axis")
-                   .attr("transform", "translate(15,180)")
-                   .call(xAxisGen);
+                   .attr("transform", "translate(10,280)")
+                   .call(xAxisGen)
+                   .selectAll("text")  
+                               .style("text-anchor", "end")
+                               .attr("dx", "-.8em")
+                               .attr("dy", ".15em")
+                               .attr("transform", function(d) {
+                                   return "rotate(-65)" 
+                                   });
 
 
                svg.append("svg:g")
                    .attr("class", "y axis")
-                   .attr("transform", "translate(30,0)")
+                   .attr("transform", "translate(40,-100)")
                    .call(yAxisGen);
 
                svg.selectAll('bar')
-                  .data(salesDataToPlot)
+                  .data(dataPlot)
                   .enter()
                   .append('rect')
-                  .attr("x",function(d,i){return (0.9*i*rawSvg.attr("width"))/(salesDataToPlot.length - 1)})
+                  .attr("x",function(d,i){return (0.94*i*rawSvg.attr("width"))/(dataPlot.length - 1)})
                   .attr('y', 200)
                   .attr('width',20)
                   .attr('height',0)
-                  .attr("transform", "translate(30,-20)")
+                  .attr("transform", "translate(40,-120)")
                   .attr('fill','steelblue')
                   .transition()
                   .delay(function(d, i) { return i * 100; })
