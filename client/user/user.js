ffbeada3387920f0b5fd88bb79806a982a2527ef
Chun Hao Wang <chunhao137@gmail.com>
2014-09-02 14:38:14 -0700

    Added d3 bar graph for users

--- client/user/user.js
+++ client/user/user.js
@@ -1,71 +1,105 @@
-angular.module('githubscout.user', [])
+angular.module('githubscout.user', ['ui.router'])
 
-.controller('UserController', ['$scope','$http', 'UserData', function($scope, $http, UserData) {
-	$scope.user = {}
+.controller('UserController', ['$scope','$http', 'User', function($scope, $http, User) {
+	$scope.userdata =[];
+	$scope.userdata.data = User.data
+}])
 
-	// These are temporary, just to make sure we get the data, but we should do something more interesting with it in the future!
-	$scope.user.rawDataCommitCount = UserData.rawDataCommitCount;
-	$scope.user.rawDataCommitsByLanguage = UserData.rawDataCommitsByLanguage
-}]);
+.directive('linearChart', function($window){
+   return{
+      restrict:'EA',
+      template:"<svg width='850' height='200'></svg>",
+       link: function(scope, elem, attrs){
+           var salesDataToPlot=scope.userdata.data;
 
+          console.log("this is the scope",salesDataToPlot)
+           var padding = 20;
+           var pathClass="path";
+           var xScale, yScale, xAxisGen, yAxisGen
 
+           var d3 = $window.d3;
+           console.log("window",$window)
 
+           var rawSvg=elem.find('svg');
+           var svg = d3.select(rawSvg[0]);
+          // console.log("rawSVG",rawSvg[0])
 
+           //setting up the axis and labeling it
 
+           function setChartParameters(){
 
-	// d3.json("data.json",function(data){
-	// 	var width = 1000;
-	//     var height =480;
-	//     var canvas = d3.select("body").append("svg")
-	// 	              .attr('width',width)
-	// 	              .attr('height',height)
-	// 	              .append('g')
-	// 	              .attr('transform','translate(150,-30)')
+               xScale = d3.scale.linear()
+                   .domain([0, salesDataToPlot.length-1])
+                   .range([padding + 5, rawSvg.attr("width") - padding]);
 
-	// 	canvas.append('text')
-	// 	        .attr("x", (width / 2))
-	// 	        .attr("y", 150)
-	// 	        .attr("text-anchor", "middle")
-	// 	        .style("font-size", "30px")
-	// 	        .style("text-decoration", "underline")
-	// 	        .text("Users Commit Data");
+               yScale = d3.scale.linear()
+                   .domain([0, d3.max(salesDataToPlot, function (d) {
 
+                       return d.count;
+                   })])
+                   .range([rawSvg.attr("height") - padding, 0]);
 
-	//            canvas.selectAll('rect')
-	//               .data(data)
-	//               .enter()
-	//                  .append('rect')
-	//                  .attr('height',function(d){return ((d.population/2.6)*0.365)*0.584/10000 })
-	//                  .attr('width',30)
-	//                  .attr('y',function(d){ return height-((d.population/2.6)*0.365)*0.584/10000 })
-	//                  .attr("x",function(d,i){return i*100 })
-	//                  .attr('fill','blue')
+               xAxisGen = d3.svg.axis()
+                   .scale(xScale)
+                   .tickFormat(function(d,i) { return salesDataToPlot[d].date})
+                   .orient("bottom")
+                   .ticks(salesDataToPlot.length - 1);
 
-	//             canvas.selectAll('text')
-	//                  .data(data)
-	//                  .enter()
-	//                  .append('text')
-	//                  .text(function(d){return Math.floor(((d.population/2.6)*0.365)*0.584)})
-	//                  .attr('x',function(d,i){
-	//                  	return i * (width/data.length)
-	//                  })
-	//                  .attr('y',function(d){
-	//                  	return height - (((d.population/2.6)*0.365)*0.584/10000)*1.05
-	//                  })
-
-	//         var x = d3.scale.linear()
-	//            .range([0,width/1.084])
-	//            .domain([0,data.length-1]);
-
-	//         var xAxis = d3.svg.axis()
-	//             .scale(x)
-	//             .tickFormat(function(d) { return data[d].county })
-	//             .orient("bottom");
-
-	//          canvas.append('g')
- //                .attr('transform','translate(0,' + height + ')')
-	//             .call(xAxis)
+                   console.log("xscale",xAxisGen)
 
 
-	// })
+               yAxisGen = d3.svg.axis()
+                   .scale(yScale)
+                   .orient("left")
+                   .ticks(4)
+            
+
+               // lineFun = d3.svg.line()
+               //     .x(function (d) {
+               //         return xScale(d.hour);
+               //     })
+               //     .y(function (d) {
+               //         return yScale(d.sales);
+               //     })
+               //     .interpolate("basis");
+           }
+         
+         function drawLineChart() {
+
+
+               setChartParameters();
+
+               svg.append("svg:g")
+                   .attr("class", "x axis")
+                   .attr("transform", "translate(15,180)")
+                   .call(xAxisGen);
+
+
+               svg.append("svg:g")
+                   .attr("class", "y axis")
+                   .attr("transform", "translate(30,0)")
+                   .call(yAxisGen);
+
+               svg.selectAll('bar')
+                  .data(salesDataToPlot)
+                  .enter()
+                  .append('rect')
+                  .attr("x",function(d,i){return (0.9*i*rawSvg.attr("width"))/(salesDataToPlot.length - 1)})
+                  .attr('y', 200)
+                  .attr('width',20)
+                  .attr('height',0)
+                  .attr("transform", "translate(30,-20)")
+                  .attr('fill','steelblue')
+                  .transition()
+                  .delay(function(d, i) { return i * 100; })
+                  .duration(500)
+                  .attr('y',function(d){ return rawSvg.attr("height")-d.count*20})
+                  .attr('height',function(d,i){return d.count*20 })
+                 
+           }
+
+           drawLineChart();
+       }
+   };
+});
 
--- server/handler.js
+++ server/handler.js
@@ -10,85 +10,34 @@ github.authenticate(config);
 
 var handler = {};
 
-//This is called by server.js as a request handler for
-//post requests to url: /api/user/commitcounts
 
 handler.getUserCommitCountsByDate = function(req,res) {
-
-  //grab username from request
-
   var username = req.body.username;
-
-  //makes a github API call that results in an array of all
-  //repos for a given user, and then invokes a callback on the result 
-
   github.repos.getFromUser({user: username},function(err,result) {
-
-    //define number of repos
-
     var numRepos = result.length;
     var numCommits;
-
-    //define counts storage object & commits storage array
-
     var counts = {};
     var commits = [];
-
-    //for every repo, do the following:
-
-    for (var i = 0; i < numRepos; i++) {
-
-      //lock repoNum into closure scope using immediately invoked function so that
-      //API calls within the for loop retain access to the correct repoNum 
-
-      (function (repoNum) {
-
-        //grab every commit in the given repo 
-
+    for (var i = 0; i < numRepos; i++) {  
+      (function (repoNum) { 
         github.repos.getCommits({user:username,repo:result[repoNum].name,author:username,per_page:100},function(err,result) {
-          
-          //define the number of commits
-          
           numCommits = result.length;
           var date;
-
-          //for ever commit in the given repo:
-
           for (var j = 0; j < numCommits; j++) {
-
-            //grab the date of the commit
-
             date = result[j].commit.committer.date.slice(0,10);
-
-            //store the date as a key:value pair in the counts object,
-            //where the key is the date and the value is the commit count
-            //if the date already exists, increment the count by 1, 
-
             counts[date] = counts[date] || 0;
             counts[date]++;
           }
-
-          //after every repo has been iterated over:
-
           if (repoNum === (numRepos-2)) {
             var commit;
-
-            //iterate over counts, and make a commit object for each date 
-            //that looks like this: {date: ___, count: ____}
-            //then push each commit object into the commits array
-
             for (var key in counts) {
               commit = {};
               commit.date = key;
               commit.count = counts[key];
               commits.push(commit);
             }
-
-            //sort the commits array by date, wrap it in JSON and respond to client
-            
             commits = commits.sort(function(a,b){ return a.date > b.date ? 1 : -1});
             res.json({results: commits});
-            
             // console.log(commits)
           }
         }); 
@@ -97,55 +46,18 @@ handler.getUserCommitCountsByDate = function(req,res) {
   });
 };
 
-//This is called by server.js as a request handler for
-//post requests to url: /api/user/commitsLanguage
-//It responds to the client with a JSON array containing every commit 
-//a given user has ever made. Each commit is represented by 
-//an object that looks like this: {date:____, languages:____, repo:_____}
-
 handler.getUserCommitsByDateAndLanguage = function(req,res) {
-
-  //grab username from post request
-
   var username = req.body.username;
-
-  //makes a github API call that results in an array of all
-  //repos for a given user, and then invokes a callback on the result 
-
   github.repos.getFromUser({user: username},function(err,result) {
-    
-    //define number of repos 
-    
     var numRepos = result.length;
     var numCommits;
-
-    //define a commits storage array, which we will later populate with
-    //every commit the user has ever made
-
     var commits = [];
     var processed = 0;
-
-    //For every repo, do the following:
-
-    for (var i = 0; i < numRepos; i++) {
-
-      //use immediately invoked function to lock repoNum and repoName into 
-      //closure scopes so that Async API calls within the for loop will refer
-      //to the correct repos
-
+    for (var i = 0; i < numRepos; i++) {  
       (function (repoNum, repoName) { 
-
-        //grab every language in the repo from githubAPI,
-        //and store the results in var languages
-
         github.repos.getLanguages({user:username,repo:repoName}, function(err,result) {
           delete result.meta;
           var languages = result;
-
-          //after the languages have been pulled from githubAPI, grab every single
-          //commit the user has made for the given repo, and store each one as an
-          //object in the commits array with date, language, and repoName properties. 
-
           github.repos.getCommits({user:username,repo:repoName,author:username,per_page:100},function(err,result) {
             numCommits = result.length;
             var date;
@@ -153,13 +65,6 @@ handler.getUserCommitsByDateAndLanguage = function(req,res) {
               date = result[j].commit.committer.date.slice(0,10);
               commits.push({date:date, languages:languages, repo:repoName});
             }
-            //after the for loop finishes, our commits array will have an 
-            //object for every commit the user has made for one given repo. Then, we 
-            //go back up to the top for loop, and repeat the process for the next repo.
-
-            //Only after every repo has been added to the commits array, we sort
-            //the array, wrap it in JSON, and respond back to the client. 
-
             processed++;
             if (processed === numRepos) {
               commits = commits.sort(function(a,b){ return a.date > b.date ? 1 : -1});
