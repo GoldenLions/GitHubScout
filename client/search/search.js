ad424396d91eeaf7da1da3f3d2086ac91f9e8db3
walkerkellywalker@gmail.com <walkerkellywalker@gmail.com>
2014-09-02 13:46:49 -0700

    (fix) can search for username from any page

--- client/search/search.js
+++ client/search/search.js
@@ -1,8 +1,8 @@
 angular.module('githubscout.search', [])
 
-.controller('SearchController', ['$scope', '$state', 'UserSearch', 'UserData', function ($scope, $state, UserSearch, UserData) {
+.controller('SearchController', ['$scope', '$state', '$stateParams', 'UserSearch', 'UserData', function ($scope, $state, $stateParams, UserSearch, UserData) {
 	$scope.input = {};
-
+	$scope.input.username= 'kwalker3690'
 	// This function finds the data for a given username, which is stored at $scope.input.username. It stores the resulting data in the UserData service, then routes to the user state
 	$scope.searchUser = function () {
 		// First send a POST request to get the user's commit count data
@@ -13,8 +13,10 @@ angular.module('githubscout.search', [])
 				// After receiving data for first POST request, send a second POST request to get the user's commit data by language
 				UserSearch.getUserCommitsByLanguage({username: $scope.input.username})
 					.then(function (data) {
+						UserData.username = $scope.input.username;
 						UserData.rawDataCommitsByLanguage = data;
-						$state.go('user')
+						$stateParams.username = $scope.input.username;
+						$state.go('user', $stateParams.username)
 					})
 			})
 	};
