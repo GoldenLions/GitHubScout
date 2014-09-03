fcf8da2f6c020871621bd69bc3309f3e581f5c08
browles <rowlesben@gmail.com>
2014-09-02 22:13:14 -0700

    (feat) Get User commit by language, using Angular

--- client/services/services.js
+++ client/services/services.js
@@ -1,13 +1,13 @@
 angular.module('githubscout.services', [])
 
-.factory('UserSearch', function ($http, $stateParams, $state, getUserStats) {
+.factory('UserSearch', function ($http, $stateParams, $state, getUserCommits) {
 
 	// Takes an object with a key of username that has a corresponding string, which is a username of a user on github. Returns an array of result JSON objects in the form of {date: 2013-03-13, count: 4}
-	var getUserCommitCount = getUserStats
+	var getUserCommitCount = getUserCommits
 
 
 	// Takes an object with a key of username that has a corresponding string, which is a username of a user on github. Returns an array of result JSON objects in the form of {"date":"2013-05-14","languages":{"JavaScript":156871,"CSS":8123}, "repo":"portfolio"}
-	var getUserCommitsByLanguage = getUserStats
+	var getUserCommitsByLanguage = getUserCommits
 
 
 	return {
@@ -19,21 +19,24 @@ angular.module('githubscout.services', [])
 .factory('UserData', function() {
 	return {}
 })
-.factory('getUserStats', function($http) {
-	var config = {
-		client_key: 'bf7e0962f270bf033f78',
-		client_secret: '37101e5bd7a17da01dfd4cb4f2889b8371b14388'
-	};
+
+
+
+.factory('getUserCommits', function($http) {
+	var config = [
+		'?client_id=bf7e0962f270bf033f78',
+		'client_secret=37101e5bd7a17da01dfd4cb4f2889b8371b14388'
+	];
 
 	var iterativeGetRepoCommits = function(repo,author,storage,page) {
-		var data = config;
-		data.page = page;
-		data.author = author;
+		var data = config.slice(0);
+		data.push('page='+page);
+		data.push('author='+author);
+		data.push('per_page=100')
 		console.log('CURRENT COMMITS',repo.commits_url, page)
 		return $http({
 			'method': 'GET',
-			'url': repo.commits_url,
-			'data': data
+			'url': repo.commits_url+data.join('&')
 		})
 		.then(function(res) {
 			console.log(res)
@@ -43,10 +46,9 @@ angular.module('githubscout.services', [])
 					'date':item.commit.committer.date.slice(0,10)
 				})
 			});
-			if (res.data.length === 30 && page < 5) {
+			if (res.data.length === 100 && page < 3) {
 				return iterativeGetRepoCommits(repo,author,storage,page+1);
 			} else {
-				console.log("Last page checked:",page)
 				return storage;
 			}
 		})
@@ -58,8 +60,7 @@ angular.module('githubscout.services', [])
 		var languages = {};
 		return $http({
 			'method': 'GET',
-			'url': repo.languages_url,
-			'data': config
+			'url': repo.languages_url+config.join('&')
 		})
 		.then(function(res) {
 			console.log(res)
@@ -80,13 +81,12 @@ angular.module('githubscout.services', [])
 		})
 	};
 
-	var getUserStats = function(obj) {
+	var getUserCommits = function(obj) {
 		var author = obj.username;
 		console.log(author)
 		return $http({
 			'method': 'GET',
-			'url': 'https://api.github.com/users/'+author+'/repos',
-			'data': config
+			'url': 'https://api.github.com/users/'+author+'/repos'+config.join('&')+'&per_page=1000'
 		})
 		.then(function(res) {
 			var data = res.data;
@@ -101,11 +101,14 @@ angular.module('githubscout.services', [])
 			return iterativeGetRepoStats(repoData,author,[])
 			.then(function(result) {
 				return result.sort(function(a,b) {
-					return a.date > b.date ? 1 : -1;
+					return a.date > b.date ? -1 : 1;
 				})
 			})
 		});
 	}
 
-	return getUserStats;
+	return getUserCommits;
 })
+
+
+
--- client/user/user.js
+++ client/user/user.js
@@ -1,24 +1,24 @@
 angular.module('githubscout.user', ['ui.router'])
 
-.controller('UserController', ['$scope','$http', 'UserData', function($scope, $http, UserData) {
+.controller('UserController', ['$scope','$http', 'User', function($scope, $http, User) {
 	$scope.userdata =[];
-	$scope.userdata.data = UserData.data
+	$scope.userdata.data = User.data
 }])
 
 .directive('linearChart', function($window){
    return{
       restrict:'EA',
-      template:"<svg width='1200' height='400'></svg>",
+      template:"<svg width='850' height='200'></svg>",
        link: function(scope, elem, attrs){
-           var dataPlot=scope.userdata.data;
+           var salesDataToPlot=scope.userdata.data;
 
-          console.log("this is the scope",dataPlot)
+          console.log("this is the scope",salesDataToPlot)
            var padding = 20;
            var pathClass="path";
            var xScale, yScale, xAxisGen, yAxisGen
 
            var d3 = $window.d3;
-           //console.log("window",$window)
+           console.log("window",$window)
 
            var rawSvg=elem.find('svg');
            var svg = d3.select(rawSvg[0]);
@@ -29,32 +29,41 @@ angular.module('githubscout.user', ['ui.router'])
            function setChartParameters(){
 
                xScale = d3.scale.linear()
-                   .domain([0, dataPlot.length-1])
+                   .domain([0, salesDataToPlot.length-1])
                    .range([padding + 5, rawSvg.attr("width") - padding]);
 
                yScale = d3.scale.linear()
-                   .domain([0, d3.max(dataPlot, function (d) {
+                   .domain([0, d3.max(salesDataToPlot, function (d) {
 
-                       return (d.count*2.4);
+                       return d.count;
                    })])
                    .range([rawSvg.attr("height") - padding, 0]);
 
                xAxisGen = d3.svg.axis()
                    .scale(xScale)
-                   .tickFormat(function(d,i) { return dataPlot[d].date})
+                   .tickFormat(function(d,i) { return salesDataToPlot[d].date})
                    .orient("bottom")
-                   .ticks(dataPlot.length - 1);
+                   .ticks(salesDataToPlot.length - 1);
 
-                   //console.log("xscale",xAxisGen)
+                   console.log("xscale",xAxisGen)
 
 
                yAxisGen = d3.svg.axis()
                    .scale(yScale)
                    .orient("left")
-                   .ticks(5)
+                   .ticks(4)
+            
 
+               // lineFun = d3.svg.line()
+               //     .x(function (d) {
+               //         return xScale(d.hour);
+               //     })
+               //     .y(function (d) {
+               //         return yScale(d.sales);
+               //     })
+               //     .interpolate("basis");
            }
-
+         
          function drawLineChart() {
 
 
@@ -62,38 +71,31 @@ angular.module('githubscout.user', ['ui.router'])
 
                svg.append("svg:g")
                    .attr("class", "x axis")
-                   .attr("transform", "translate(10,280)")
-                   .call(xAxisGen)
-                   .selectAll("text")
-                               .style("text-anchor", "end")
-                               .attr("dx", "-.8em")
-                               .attr("dy", ".15em")
-                               .attr("transform", function(d) {
-                                   return "rotate(-65)"
-                                   });
+                   .attr("transform", "translate(15,180)")
+                   .call(xAxisGen);
 
 
                svg.append("svg:g")
                    .attr("class", "y axis")
-                   .attr("transform", "translate(40,-100)")
+                   .attr("transform", "translate(30,0)")
                    .call(yAxisGen);
 
                svg.selectAll('bar')
-                  .data(dataPlot)
+                  .data(salesDataToPlot)
                   .enter()
                   .append('rect')
-                  .attr("x",function(d,i){return (0.94*i*rawSvg.attr("width"))/(dataPlot.length - 1)})
+                  .attr("x",function(d,i){return (0.9*i*rawSvg.attr("width"))/(salesDataToPlot.length - 1)})
                   .attr('y', 200)
                   .attr('width',20)
                   .attr('height',0)
-                  .attr("transform", "translate(40,-120)")
+                  .attr("transform", "translate(30,-20)")
                   .attr('fill','steelblue')
                   .transition()
                   .delay(function(d, i) { return i * 100; })
                   .duration(500)
                   .attr('y',function(d){ return rawSvg.attr("height")-d.count*20})
                   .attr('height',function(d,i){return d.count*20 })
-
+                 
            }
 
            drawLineChart();
