119ee943d9f45cc4b6454b5b573664dbfcf57496
Ben Rowles <rowlesben@gmail.com>
2014-09-02 22:34:02 -0700

    fix?

--- client/user/user.js
+++ client/user/user.js
@@ -1,105 +1,33 @@
-angular.module('githubscout.user', ['ui.router'])
+angular.module('githubscout.search', [])
 
-.controller('UserController', ['$scope','$http', 'User', function($scope, $http, User) {
-	$scope.userdata =[];
-	$scope.userdata.data = User.data
+.controller('SearchController', ['$scope', '$state', '$stateParams', 'UserSearch', 'UserData', function ($scope, $state, $stateParams, UserSearch, UserData) {
+  $scope.input = {};
+  $scope.input.username= 'kwalker3690'
+  // This function finds the data for a given username, which is stored at $scope.input.username. It stores the resulting data in the UserData service, then routes to the user state
+  $scope.searchUser = function () {
+    // First send a POST request to get the user's commit count data
+    UserSearch.getUserCommitCount({username: $scope.input.username})
+    .then(function (data) {
+      UserData.rawDataCommitCount = data;
+
+        // After receiving data for first POST request, send a second POST request to get the user's commit data by language
+        UserSearch.getUserCommitsByLanguage({username: $scope.input.username})
+        .then(function (data) {
+          UserData.username = $scope.input.username;
+          UserData.rawDataCommitsByLanguage = data;
+          $stateParams.username = $scope.input.username;
+          $state.go('user', $stateParams.username)
+        })
+      })
+  };
+
+
 }])
 
-.directive('linearChart', function($window){
-   return{
-      restrict:'EA',
-      template:"<svg width='850' height='200'></svg>",
-       link: function(scope, elem, attrs){
-           var salesDataToPlot=scope.userdata.data;
-
-          console.log("this is the scope",salesDataToPlot)
-           var padding = 20;
-           var pathClass="path";
-           var xScale, yScale, xAxisGen, yAxisGen
-
-           var d3 = $window.d3;
-           console.log("window",$window)
-
-           var rawSvg=elem.find('svg');
-           var svg = d3.select(rawSvg[0]);
-          // console.log("rawSVG",rawSvg[0])
-
-           //setting up the axis and labeling it
-
-           function setChartParameters(){
-
-               xScale = d3.scale.linear()
-                   .domain([0, salesDataToPlot.length-1])
-                   .range([padding + 5, rawSvg.attr("width") - padding]);
-
-               yScale = d3.scale.linear()
-                   .domain([0, d3.max(salesDataToPlot, function (d) {
-
-                       return d.count;
-                   })])
-                   .range([rawSvg.attr("height") - padding, 0]);
-
-               xAxisGen = d3.svg.axis()
-                   .scale(xScale)
-                   .tickFormat(function(d,i) { return salesDataToPlot[d].date})
-                   .orient("bottom")
-                   .ticks(salesDataToPlot.length - 1);
-
-                   console.log("xscale",xAxisGen)
-
-
-               yAxisGen = d3.svg.axis()
-                   .scale(yScale)
-                   .orient("left")
-                   .ticks(4)
-            
-
-               // lineFun = d3.svg.line()
-               //     .x(function (d) {
-               //         return xScale(d.hour);
-               //     })
-               //     .y(function (d) {
-               //         return yScale(d.sales);
-               //     })
-               //     .interpolate("basis");
-           }
-         
-         function drawLineChart() {
-
-
-               setChartParameters();
-
-               svg.append("svg:g")
-                   .attr("class", "x axis")
-                   .attr("transform", "translate(15,180)")
-                   .call(xAxisGen);
-
-
-               svg.append("svg:g")
-                   .attr("class", "y axis")
-                   .attr("transform", "translate(30,0)")
-                   .call(yAxisGen);
-
-               svg.selectAll('bar')
-                  .data(salesDataToPlot)
-                  .enter()
-                  .append('rect')
-                  .attr("x",function(d,i){return (0.9*i*rawSvg.attr("width"))/(salesDataToPlot.length - 1)})
-                  .attr('y', 200)
-                  .attr('width',20)
-                  .attr('height',0)
-                  .attr("transform", "translate(30,-20)")
-                  .attr('fill','steelblue')
-                  .transition()
-                  .delay(function(d, i) { return i * 100; })
-                  .duration(500)
-                  .attr('y',function(d){ return rawSvg.attr("height")-d.count*20})
-                  .attr('height',function(d,i){return d.count*20 })
-                 
-           }
-
-           drawLineChart();
-       }
-   };
-});
+.directive('gsSearch', function () {
+  return {
+    restrict: 'A',
+    templateUrl: 'search/search.html'
+  }
+})
 
