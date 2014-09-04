2a8db5c73c61b585f42061bf2630e5925bb4d141
wykhuh <wykhuh@users.noreply.github.com>
2014-09-04 08:03:06 -0700

    (feat/languagePage) Start incorporating language charts into Angular.

--- client/language/language.js
+++ client/language/language.js
@@ -1,5 +1,34 @@
-angular.module('githubscout.language', [])
+angular.module('githubscout.language', ['nvd3ChartDirectives', 'ui.router'])
 
-.controller('LanguageController', ['$scope', function ($scope){
+  .controller('LanguageController', ['$scope', function($scope){
 
-}])
+    $scope.commits = [{
+      "key": "Github Api Mean Web Response Time",
+      values: [[1,1], [3,3]]
+    }];
+
+    $scope.creates = [{
+      "key": "Github Api Mean Web Response ",
+      values: [[5,5], [3,3]]
+    }];
+
+
+    $scope.public_repos = [{
+      "key": "Github Api Mean Web Response ",
+      values: [[5,5], [3,3]]
+    }];
+
+
+    $scope.pushes = [{
+      "key": "Github Api Mean Web Response ",
+      values: [[5,5], [3,3]]
+    }];
+
+
+  }])
+  
+
+  
+
+
+  
