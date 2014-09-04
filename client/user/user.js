613885781e1c2894d45b52b1553b92341dc3f9a5
Chun Hao Wang <chunhao137@gmail.com>
2014-09-03 22:59:50 -0700

    Added functions to count user commits by date and languages. Graph now displays user commits by date and commits by languages.

--- client/user/user.js
+++ client/user/user.js
@@ -1,38 +1,68 @@
-var user = angular.module('githubscout.user', ['ui.router'])
+var userapp = angular.module('githubscout.user', ['ui.router'])
 
-user.controller('UserController', ['$scope','$http', 'UserData', function($scope, $http, UserData) {
+userapp.controller('UserController', ['$scope','$http', 'UserData', 'getUserCommits',function($scope, $http, UserData) {
   $scope.userdata =[];
+  $scope.userdata.data = UserData.rawDataCommitsByLanguage
 
-  $scope.userdata.data = UserData.rawDataCommitCount
-  $scope.userdata.data1 = UserData.rawDataCommitsByLanguage
-  $scope.userdata.fakedata = [
-  {"language":"javascript","count":120},
-  {"language":"ruby","count":100},
-  {"language":"python","count":70}
-  ];
-  // var dateLanguageCountperUser ={};
-  // for(var i =0; i<$scope.userdata.data1.length; i++){
-       
-  //       if(dateLanguageCountperUser[Object.keys($scope.userdata.data1[i].languages)[0]]){
-  //           dateLanguageCountperUser[Object.keys($scope.userdata.data1[i].languages)[0]]++             
-  //       }else{
-  //          dateLanguageCountperUser[Object.keys($scope.userdata.data1[i].languages)[0]]=1
-  //       }
-        
-  // }
-  // console.log("this is javascriptcount",dateLanguageCountperUser)
+  // getdateandCommits will return an array of object with object having the form {date:'2014-06-04', count:5}
+  var getdateandCommits  = function(){
+    var result = []
+    var commit = {};
+   for(var i =0; i<$scope.userdata.data.length; i++){
+     var repo = $scope.userdata.data[i]
+      for(var key in repo){
+          if(key === "date"){
+             if(commit[repo[key]]){
+                commit[repo[key]]++;
+             }else{
+                commit[repo[key]]=1;
+             }
+          }
+      }
+    }
+    for(var key in commit){
+        result.push({date:key,count:commit[key]})
+    }
+    return result
+  }
+  // getUserCommitsperLanganguage will return an array of object with object having the form {language:'JavaScript', count:10}
+  var getUserCommitsperLanganguage = function(){
+      var result = []
+      var commit ={}
+      for(var i=0; i<$scope.userdata.data.length;i++){
+        var repo = $scope.userdata.data[i].languages;
+        for(var key in repo){
+             if(commit[key]){
+                commit[key]++
+             }else{
+               commit[key]=1
+             }
+         }
 
+      }
+      for(var key in commit){
+         result.push({language:key,count:commit[key]})
+      }
+      return result;
+  }
+
+  $scope.userDateandCommits = getdateandCommits().reverse()
+  $scope.userCommitsperLanguage = getUserCommitsperLanganguage()
+  
 
 }])
 
-user.directive('usercommitChart', function($window){
+//creating the d3 directive for commits for specific user
+userapp.directive('usercommitChart', function($window){
    return{
       restrict:'EA',
-      template:"<svg width='1200' height='400'></svg>",
+      template:"<svg width='1200' height='600'></svg>",
        link: function(scope, elem, attrs){
-           var dataPlot=scope.userdata.data;
+      
+           var dataPlot=scope.userDateandCommits
+           console.log("herrrre",dataPlot)
 
-          console.log("this is the scope",dataPlot)
+          //console.log("this is the scope",dataPlot)
            var padding = 20;
            var pathClass="path";
            var xScale, yScale, xAxisGen, yAxisGen
@@ -47,8 +77,6 @@ user.directive('usercommitChart', function($window){
            //setting up the axis and labeling it
 
            function setChartParameters(){
-              var max = d3.max(dataPlot, function(d) { return d.count });
-              console.log("maxxxxxx1",max)
 
              xScale = d3.scale.linear()
              .domain([0, dataPlot.length-1])
@@ -57,19 +85,109 @@ user.directive('usercommitChart', function($window){
              yScale = d3.scale.linear()
              .domain([0, d3.max(dataPlot, function (d) {
 
-<<<<<<< HEAD
-               return (d.count*2.4);
+               return parseInt(d.count);
              })])
-             .range([rawSvg.attr("height") - padding, 0]);
-=======
-                       return d.count
-                   })])
-                   .range([rawSvg.attr("height") - padding, 0]);
->>>>>>> Made changes to scale of axes
+             .range([400 - padding, 0]);
 
              xAxisGen = d3.svg.axis()
              .scale(xScale)
              .tickFormat(function(d,i) { return dataPlot[d].date})
+             .orient("bottom")
+             .ticks(dataPlot.length - 1);
+
+
+                   yAxisGen = d3.svg.axis()
+                   .scale(yScale)
+                   .orient("left")
+                   .ticks(5)
+
+                 }
+
+                 function drawLineChart() {
+
+
+                   setChartParameters();
+
+                   svg.append("svg:g")
+                   .attr("class", "x axis")
+                   .attr("transform", "translate(10,380)")
+                   .call(xAxisGen)
+                   .selectAll("text")
+                   .style("text-anchor", "end")
+                   .attr("dx", "-.8em")
+                   .attr("dy", ".15em")
+                   .attr("transform", function(d) {
+                     return "rotate(-65)"
+                   });
+
+
+                   svg.append("svg:g")
+                   .attr("class", "y axis")
+                   .attr("transform", "translate(40,0)")
+                   .call(yAxisGen);
+
+               svg.selectAll('bar')
+                  .data(dataPlot)
+                  .enter()
+                  .append('rect')
+                  .attr("x",function(d,i){return (0.94*i*rawSvg.attr("width"))/(dataPlot.length - 1)})
+                  .attr('width',20)
+                  .attr('height',0)
+                  .attr("transform", "translate(40,0)")
+                  .attr('fill','steelblue')
+                  .transition()
+                  .delay(function(d, i) { return i * 100; })
+                  .duration(500)
+                  .attr('y',function(d){ return yScale(d.count)})
+                  .attr('height',function(d,i){return 380-yScale(d.count) })
+
+           }
+
+           drawLineChart();
+       }
+   };
+})
+
+//creating the d3 directive commites for languages for specific user
+userapp.directive('userlangaugeChart', function($window){
+   return{
+      restrict:'EA',
+      template:"<svg width='1200' height='600'></svg>",
+       link: function(scope, elem, attrs){
+      
+           var dataPlot=scope.userCommitsperLanguage
+           console.log("herrrre",dataPlot)
+
+          //console.log("this is the scope",dataPlot)
+           var padding = 20;
+           var pathClass="path";
+           var xScale, yScale, xAxisGen, yAxisGen
+
+           var d3 = $window.d3;
+           //console.log("window",$window)
+
+           var rawSvg=elem.find('svg');
+           var svg = d3.select(rawSvg[0]);
+          // console.log("rawSVG",rawSvg[0])
+
+           //setting up the axis and labeling it
+
+           function setChartParameters(){
+
+             xScale = d3.scale.linear()
+             .domain([0, dataPlot.length-1])
+             .range([padding + 5, rawSvg.attr("width") - padding]);
+
+             yScale = d3.scale.linear()
+             .domain([0, d3.max(dataPlot, function (d) {
+
+               return parseInt(d.count);
+             })])
+             .range([400 - padding, 0]);
+
+             xAxisGen = d3.svg.axis()
+             .scale(xScale)
+             .tickFormat(function(d,i) { return dataPlot[d].language})
              .orient("bottom")
              .ticks(dataPlot.length - 1);
 
@@ -83,18 +201,14 @@ user.directive('usercommitChart', function($window){
 
                  }
 
-<<<<<<< HEAD
                  function drawLineChart() {
-=======
-         function drawBarChart() {
->>>>>>> Made changes to scale of axes
 
 
                    setChartParameters();
 
                    svg.append("svg:g")
                    .attr("class", "x axis")
-                   .attr("transform", "translate(10,280)")
+                   .attr("transform", "translate(10,380)")
                    .call(xAxisGen)
                    .selectAll("text")
                    .style("text-anchor", "end")
@@ -107,119 +221,27 @@ user.directive('usercommitChart', function($window){
 
                    svg.append("svg:g")
                    .attr("class", "y axis")
-                   .attr("transform", "translate(40,-100)")
+                   .attr("transform", "translate(40,0)")
                    .call(yAxisGen);
 
-               svg.selectAll('rect')
+               svg.selectAll('bar')
                   .data(dataPlot)
                   .enter()
                   .append('rect')
-       
                   .attr("x",function(d,i){return (0.94*i*rawSvg.attr("width"))/(dataPlot.length - 1)})
-                  .attr('y', 200)
                   .attr('width',20)
                   .attr('height',0)
+                  .attr("transform", "translate(40,0)")
+                  .attr('fill','steelblue')
                   .transition()
                   .delay(function(d, i) { return i * 100; })
                   .duration(500)
-                  .attr('y',function(d){ return rawSvg.attr("height")-yScale(d.count)})
-                  .attr('height',function(d,i){return yScale(d.count) })
-                  .attr("transform", "translate(40,-120)")
-                  .attr('fill','steelblue')
-                 
+                  .attr('y',function(d){ return yScale(d.count)})
+                  .attr('height',function(d,i){return 380-yScale(d.count) })
+
            }
 
-           drawBarChart();
+           drawLineChart();
        }
    };
 })
-user.directive('userlangaugeChart', function($window){
-   return{
-      restrict:'EA',
-      template:"<svg width='1200' height='400'></svg>",
-       link: function(scope, elem, attrs){
-           var dataPlot=scope.userdata.fakedata;
-
-          console.log("this is the scope",dataPlot)
-           var padding = 20;
-           var pathClass="path";
-           var xScale, yScale, xAxisGen, yAxisGen
-
-           var d3 = $window.d3;
-           var rawSvg=elem.find('svg');
-           var svg = d3.select(rawSvg[0]);
-           //setting up the axis and labeling it
-
-           function setChartParameters(){
-
-               xScale = d3.scale.linear()
-                   .domain([0, dataPlot.length-1])
-                   .range([padding + 5, rawSvg.attr("width") - padding]);
-
-               yScale = d3.scale.linear()
-                   .domain([0, d3.max(dataPlot, function (d) {
-
-                       return (d.count);
-                   })])
-                   .range([rawSvg.attr("height") - padding, 0]);
-
-               xAxisGen = d3.svg.axis()
-                   .scale(xScale)
-                   .tickFormat(function(d,i) { return dataPlot[d].language})
-                   .orient("bottom")
-                   .ticks(dataPlot.length - 1);
-
-
-               yAxisGen = d3.svg.axis()
-                   .scale(yScale)
-                   .orient("left")
-                   .ticks(5)
-
-           }
-
-         function drawBarChart() {
-
-
-               setChartParameters();
-
-               svg.append("svg:g")
-                   .attr("class", "x axis")
-                   .attr("transform", "translate(10,280)")
-                   .call(xAxisGen)
-                   .selectAll("text")
-                               .style("text-anchor", "end")
-                               .attr("dx", "-.8em")
-                               .attr("dy", ".15em")
-                               .attr("transform", function(d) {
-                                   return "rotate(-65)"
-                                   });
-
-
-               svg.append("svg:g")
-                   .attr("class", "y axis")
-                   .attr("transform", "translate(40,-100)")
-                   .call(yAxisGen);
-
-               svg.selectAll('rect')
-                  .data(dataPlot)
-                  .enter()
-                  .append('rect')
-       
-                  .attr("x",function(d,i){return (0.94*i*rawSvg.attr("width"))/(dataPlot.length - 1)})
-                  .attr('y', 200)
-                  .attr('width',20)
-                  .attr('height',0)
-                  .transition()
-                  .delay(function(d, i) { return i * 100; })
-                  .duration(500)
-                  .attr('y',function(d){ return rawSvg.attr("height")- yScale(d.count)})
-                  .attr('height',function(d,i){return  yScale(d.count) })
-                  .attr("transform", "translate(40,-120)")
-                  .attr('fill','steelblue')
-                 
-           }
-
-           drawBarChart();
-       }
-   };
-});
