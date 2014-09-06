b35b004ffe637f8ce960c9098a18190fdce87623
wykhuh <wykhuh@users.noreply.github.com>
2014-09-05 20:27:27 -0700

    (feat/homePage) Add top 10 and all languages charts.

--- client/home/home.js
+++ client/home/home.js
@@ -3,6 +3,17 @@ angular.module('githubscout.home', ['nvd3ChartDirectives'])
 .controller('HomeController', [ '$scope', 'ChartsUtil', function($scope, ChartsUtil){
 
   settings = {
+    countType: 'activity',
+    url: './CSVs/top_repo_by_activity_quarterly.csv'
+  };
+
+  ChartsUtil.fetchStackedAreaData(settings)
+    .then(function(chartData){
+      console.log('chartData', chartData)
+      $scope.topLanguages  = chartData;
+  });
+
+  settings = {
     countType: 'commits',
     url: './CSVs/language_all_time1.csv',
     key: 'All Languages'
@@ -10,12 +21,8 @@ angular.module('githubscout.home', ['nvd3ChartDirectives'])
 
   ChartsUtil.fetchHorizontalBarData(settings)
     .then(function(chartData){
-      console.log('chartData', chartData);
       $scope.allLanguages  = chartData;
-
   });
-
-
 
   // // Formats the JavaScript date object for the x axis labels
   $scope.xAxisTickFormat = function(){
@@ -24,10 +31,5 @@ angular.module('githubscout.home', ['nvd3ChartDirectives'])
       };
   };
 
-  $scope.barHeight = function(){
-    return function(d,i){
-      return (i *10 +'px');
-    }
-  }
 
 }])
