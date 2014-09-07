c7524da7bcec0bab1ca8e0374d5038e087251d34
browles <rowlesben@gmail.com>
2014-09-06 23:42:12 -0700

    cleanup

--- server/scraper.js
+++ server/scraper.js
@@ -1,85 +1,86 @@
 var request = require('superagent');
 var cheerio = require('cheerio');
-var Batch = require('batch');
+var async = require('async');
 var _ = require('lodash');
 var fs = require('fs');
 
 var languages = require('./languages.js')
 
+
+
 // Utility functions.
 var utils = {
-  // Uses batch to perform 10 GET requests at a time.
+  // Uses async to perform 10 GET requests at a time.
   // Calls progressback on each response body, placing the result in an accumulating results array.
   // Expects objs to be an array of objects with the .url property.
   // Upon completion, calls callback on the finished results array.
   batchGet: function(objs,progressback,callback) {
-    var batch = new Batch();
-    batch.concurrency(10);
-    _.each(objs, function(obj) {
-      batch.push(function(done) {
+    var progress = 0;
+    async.mapLimit(objs, 10, function(obj, done) {
+      request
+      .get(obj.url)
+      .set('User-Agent', 'curl/7.24.0 (x86_64-apple-darwin12.0) libcurl/7.24.0 OpenSSL/0.9.8r zlib/1.2.5')
+      .end(function(error,response) {
+        if (error) console.log( new Error(error) );
+        if (response.error) console.log(response.status,response.error);
+        console.log('Progress: ',++progress+'/'+objs.length);
+        var result = progressback(response.text,obj);
+        done(error,result);
+      });
+    }, callback);
+  },
+  // One GET request at least every delay milliseconds.
+  // Expects objs to be an array of objects with the .url property.
+  // Not using async at the moment because it isn't clear how to throttle... 
+  throttledBatchGet: function(objs,progressback,callback,delay) {
+    var progress = 0;
+    var results = [];
+    var queue = _.map(objs, function(obj) {
+      return function() {
         request
         .get(obj.url)
         .set('User-Agent', 'curl/7.24.0 (x86_64-apple-darwin12.0) libcurl/7.24.0 OpenSSL/0.9.8r zlib/1.2.5')
         .end(function(error,response) {
-          if (error) throw new Error(error);
+          if (error) console.log( new Error(error) );
           if (response.error) console.log(response.status,response.error);
+          console.log('Progress: ',++progress+'/'+objs.length);
           var result = progressback(response.text,obj);
-          done(error,result);
-        });
-      });
+          results.push(result);
+        }); 
+      };
     });
-    batch.on('progress', function(e){
-      console.log(e);
-    });
-    batch.end(function(error,results) {
-      if (error) throw new Error(error);
-      callback(results);
-    });
-  },
-  // One GET request at least every delay milliseconds.
-  // Expects objs to be an array of objects with the .url property.
-  throttledBatchGet: function(objs,progressback,callback,delay) {
-    var batch = new Batch();
-    _.each(objs, function(obj, i) {
-      batch.push(function(done) {
-        setTimeout(function() {
-          request
-          .get(obj.url)
-          .set('User-Agent', 'curl/7.24.0 (x86_64-apple-darwin12.0) libcurl/7.24.0 OpenSSL/0.9.8r zlib/1.2.5')
-          .end(function(error,response) {
-            if (error) throw new Error(error);
-            if (response.error) console.log(response.status,response.error);
-            var result = progressback(response.text,obj);
-            done(error,result);
-          }); 
-        }, delay*i);
-      });
-    });
-    batch.on('progress', function(e){
-      console.log(e);
-    });
-    batch.end(function(error,results) {
-      if (error) throw new Error(error);
-      callback(results);
-    });
+
+    queue.pop()();
+    var interval = setInterval(function() {
+      if (queue.length === 0 ) {
+        clearInterval(interval);
+        return callback(null, results);
+      };
+      queue.pop()();
+    },delay);
   },
   // progressback to pull the user URLs out of the response body items.
-  parseUserItems: function(response) {
-    return _.map(JSON.parse(response).items,function(item) {
+  parseUserItems: function(response,obj) {
+    var result = _.map(JSON.parse(response).items,function(item) {
       return {
         url: item.html_url
       };
     });
+    return result;
   },
   // progressback to pull the repo URL, size, and primary language out of each response body item.
-  parseRepoItems: function(response) {
-    return _.map(JSON.parse(response).items,function(item) {
+  parseRepoItems: function(response,obj) {
+    var result = _.map(JSON.parse(response).items,function(item) {
       return {
         url: item.html_url,
         size: item.size,
         language: item.language
       };
     });
+    fs.writeFileSync('./leaderboard-data/repoUrls/top-repo-urls-'+encodeURIComponent(obj.language)+'.json',
+      JSON.stringify(result,null,2));
+    console.log(obj.language,'done.');
+    return result;
   },
   // progressback that uses cheerio to parse the response body, which in this case is just HTML.
   // Pulls various figures out of the profile HTML and decorates the input obj.
@@ -104,7 +105,7 @@ var utils = {
       followers: $('.vcard-stats .vcard-stat:first-child').find('.vcard-stat-count').text()
     });
   },
-  // progressback to scrap various figures from the repo page HTML.
+  // progressback to scrape various figures from the repo page HTML.
   scrapeRepoStats: function(html,obj) {
     var $ = cheerio.load(html);
     return _.extend(obj, {
@@ -140,9 +141,9 @@ var scraper = {
       };
     });
     // Gets the 1000 top users, writes them to a .json.
-    utils.batchGet(userSearchURLS, utils.parseUserItems, function(results) {
+    utils.batchGet(userSearchURLS, utils.parseUserItems, function(error,results) {
       results = _.flatten(results);
-      fs.writeFileSync('top-user-urls.json',JSON.stringify(results,null,2));
+      fs.writeFileSync('./leaderboard-data/userUrls/top-user-urls.json',JSON.stringify(results,null,2));
       console.log('updateUserUrlsJSON finished.');
       if (callback) callback();
     });
@@ -150,17 +151,17 @@ var scraper = {
   // Updates top-user-stats.json to reflect the current stats of the top 1000 users found 
   // found previously.
   updateProfileStatsJSON: function(callback) {
-    var profileUrlObjs = JSON.parse(fs.readFileSync('top-user-urls.json'));
+    var profileUrlObjs = JSON.parse(fs.readFileSync('./leaderboard-data/userUrls/top-user-urls.json'));
     // Scrapes statistics from the profile URLs, writes the stats to a .json.
-    utils.batchGet(profileUrlObjs, utils.scrapeProfileStats, function(results) {
-      fs.writeFileSync('top-user-stats.json',JSON.stringify(results,null,2));
+    utils.batchGet(profileUrlObjs, utils.scrapeProfileStats, function(error,results) {
+      fs.writeFileSync('./leaderboard-data/userUrls/top-user-stats.json',JSON.stringify(results,null,2));
       console.log('updateProfileStatsJSON finished.');
       if (callback) callback();
     });
   },
   // Updates top-repo-urls.json to reflect the top 20 repos of each language, sorted by stars.
   // The GitHub API limits even authenticated search requests to 20 queries per minute,
-  // we use throttleBatchGet with a delay of 3 seconds to get around this. Seeing as we have ~200
+  // we use throttledBatchGet with a delay of 3 seconds to get around this. Seeing as we have ~200
   // languages to search for, this will take at least 10 minutes.
   // minStars must be set pretty low because some of the repos of the more esoteric languages
   // have very few stars, even for the top repos.
@@ -172,26 +173,38 @@ var scraper = {
     var repoSearchUrlObjs = _.map(languages, function(language) {
       return {
         url: 'https://api.github.com/search/repositories'+config
-            +'&q=stars%3A>'+minStars+'+language%3A'+encodeURIComponent(language)
+            +'&q=language%3A'+encodeURIComponent(language),
+        language: language
       };
     });
-    utils.throttledBatchGet(repoSearchUrlObjs, utils.parseRepoItems, function(results) {
-      results = _.flatten(results);
-      fs.writeFileSync('top-repo-urls.json',JSON.stringify(results,null,2));
+    utils.throttledBatchGet(repoSearchUrlObjs, utils.parseRepoItems, function(error,results) {
       console.log('updateRepoUrlsJSON finished.');
       if (callback) callback();
     }, 3000);
   },
   // Update top-repo-stats.json to reflect the current stats of the top repos per language.
   updateRepoStatsJSON: function(callback) {
-    var repoUrlObjs = JSON.parse(fs.readfileSync('top-repo-urls.json'));
-    utils.batchGet(repoUrlObjs, utils.scrapeRepoStats, function(results) {
-      fs.writeFileSync('top-repo-stats.json',JSON.stringify(results,null,2));
+    var repoUrlObjs = _.map(languages, function(language) {
+      JSON.parse(fs.readFileSync('./leaderboard-data/repoUrls/top-repo-urls-'+encodeURIComponent(language)+'.json'));
+    });
+    console.log(repoUrlObjs)
+    utils.batchGet(repoUrlObjs, utils.scrapeRepoStats, function(error,results) {
+      fs.writeFileSync('./leaderboard-data/top-repo-stats.json',JSON.stringify(results,null,2));
       console.log('updateRepoStatsJSON finished.');
       if (callback) callback();
     });
   }
 };
+
+// scraper.updateUserUrlsJSON();
+// scraper.updateProfileStatsJSON();
+// scraper.updateRepoUrlsJSON();
+// scraper.updateRepoStatsJSON();
+
+console.log(_.map(languages, function(language) {
+      JSON.parse('./leaderboard-data/repoUrls/top-repo-urls-'+encodeURIComponent(language)+'.json');
+    }))
+// console.log(JSON.parse(fs.readFileSync('leaderboard-data/repoUrls/top-repo-urls-'+encodeURIComponent('dengis')+'.json')))
 
 
 module.exports = scraper;
